3e0761b4544af3b4f1a6dbe0fefa0c654f31271d
John Doe <30752740@qq.com>
2019-10-09 18:59:53 +0800

    更新

--- gmall0508-cannal/src/main/scala/com/atguigu/canal/CanalClient.scala
+++ gmall0508-cannal/src/main/scala/com/atguigu/canal/CanalClient.scala
@@ -14,8 +14,6 @@ import com.google.protobuf.ByteString
   * Date 2019-10-09 16:38
   */
 object CanalClient {
-    
-    
     def main(args: Array[String]): Unit = {
         // 1. 建立连接
         val address: SocketAddress = new InetSocketAddress("hadoop201", 11111)
@@ -23,7 +21,6 @@ object CanalClient {
         conn.connect() // 连接到canal
         conn.subscribe("gmall.*") // 订阅要读取的数据的表
         // 2. 读数据
-        
         while (true) {
             val msg: Message = conn.get(100)
             val entries: util.List[CanalEntry.Entry] = msg.getEntries
@@ -37,14 +34,12 @@ object CanalClient {
                         val rowDataList: util.List[CanalEntry.RowData] = rowChange.getRowDatasList
                         CanalHandler.handler(entry.getHeader.getTableName, rowDataList, rowChange.getEventType)
                     }
-                    
                 }
             }else{
                 println("没有获取到数据, 2s之后重新拉取...")
                 Thread.sleep(2000)
             }
         }
-        
     }
 }
 
@@ -52,12 +47,13 @@ object CanalClient {
  // 1. 建立连接
 
 2. 通过连接读数据
-Message get一次获取一个Message, 一个Message表示一批数据, 看出多条sql语句执行的结果
-Entry  一个Message封装多个Entry, 一个Entry可以看出一条sql语句执行的多行结果
-StoreValue 一个 Entry封装一个 storeValue, 可以看到是是数据序列化形式
-RowChange 从StoreValue里面解析出来的数据类型
-RowData 一个rowChage会有多个RowData, 一个RowData封装了一行数据
-Column 列
+
+    Message get一次获取一个Message, 一个Message表示一批数据, 看出多条sql语句执行的结果
+    Entry  一个Message封装多个Entry, 一个Entry可以看出一条sql语句执行的多行结果
+    StoreValue 一个 Entry封装一个 storeValue, 可以看到是是数据序列化形式
+    RowChange 从StoreValue里面解析出来的数据类型
+    RowData 一个rowChage会有多个RowData, 一个RowData封装了一行数据
+    Column 列
 
 
 
